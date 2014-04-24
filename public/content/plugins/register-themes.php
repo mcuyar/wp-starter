0efc42e3c32c5971535cb7f68d13c6bdde8093c5
Matthew Cuyar <matt@fuelingbrands.com>
2014-04-24 12:48:36 -0400

    add public folder

---
+++ public/content/plugins/register-themes.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Register the default wordpress theme directory
+ * 
+ * @return void
+ */
+add_action('muplugins_loaded', function(){
+	register_theme_directory( trailingslashit(ABSPATH).'wp-content/themes' );
+});
